e1f4256ac00edada211e3baa14e0f5a9c6182d70
Gabriel <gabeklav@gmail.com>
2021-12-08 13:52:01 -0500

    Remove imports, it speeds up profile loading

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
-Import-Module posh-git
-Import-Module oh-my-posh
+$env:AZ_ENABLED = $false
+#Import-Module posh-git
+#Import-Module oh-my-posh
 Set-PoshPrompt -Theme paradox
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
